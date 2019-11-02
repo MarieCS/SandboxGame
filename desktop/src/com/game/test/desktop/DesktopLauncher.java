3bc7eb294c33de267a77fb66a69c1b79f38fd9f3
cokoyan <cokoyan@gmail.com>
2019-11-02 17:42:20 +0100

    Init du projet Sandbox

---
+++ desktop/src/com/game/test/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.game.test.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.game.test.SandboxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SandboxGame(), config);
+	}
+}
